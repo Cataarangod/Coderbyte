4e5adb648aff0f15d1eeb640298cc38b0ab6e618
Catalina Arango <cataarangoss@gmail.com>
2022-06-28 13:09:45 -0500

    fix split

--- NumberSearch.js
+++ NumberSearch.js
@@ -1,10 +1,14 @@
-function numberSearch(str) {
-    var separatedString = str.split('');
+function NumberSearch(str) {
+
+    // Declare variables
+    var separatedString = str.split('')
     var letters = [];
     var numbers = [];
     var sum = 0;
 
-    for (var i = 0; i < str.length; i++) {
+    /*Once we´ve created the empty arrays we can
+    now filter and push those separated Items into the lists*/
+    for (var i = 0; i < str.lenght; i++) {
         var currentItem = separatedString[i];
         if (isNaN(parseInt(currentItem))) {
             letters.push(currentItem);
@@ -13,10 +17,15 @@ function numberSearch(str) {
         };
     };
 
+    /*Now we can operate with the numbers, */
     for (var i = 0; i < numbers.length; i++) {
         currentNumber = Number(numbers[i]);
         sum = sum + currentNumber;
     };
 
     return (Math.round(sum / letters.length));
+
 };
+
+// keep this function call here 
+console.log(NumberSearch(readline()));
